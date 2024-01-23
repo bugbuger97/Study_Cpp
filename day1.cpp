3f3ad0d4cbd37abcfabedc04436f468c4f0183e7
bugbuger97 <junopaeng@gmail.com>
2024-01-23 18:47:24 +0900

    char

--- day1.cpp
+++ day1.cpp
@@ -2,14 +2,20 @@
 using namespace std;
 
 int main(){
-    unsigned short int n0 = -1; // unsigned short int -> 0 ~ 65,536
-    unsigned int n1 = -1; // unsigned int -> 0 ~ 4,294,967,295
-    cout << n0 << endl;
-    cout << n1 << endl;
-    // 정수 리터럴 : 프로그램에서 어떤 자료형의 값을 고정하여 사용.
-    cout << 1234 << endl; // int
-    cout << 1234U << endl; // unsigned int
-    cout << 1234L << endl; // long int
-    cout << 1234UL << endl; // unsigned long int
+    // 문자 자료형 : 1 byte by ASCII incoding system -> 0 ~ 127 범위의 정수를 사용해서 128개의 글자를 정의하고 있음.
+    char A;
+    char a;
+    A = 65;
+    a = 97;
+    cout << "65 of ASCII : " << A << endl;
+    cout << "97 of ASCII : " << a << endl;
+
+    // 문자 리터널 
+    // -> method 1 : ASCII Table 안의 글자를 작은 따옴표 안에 넣어 사용함.
+    char alphabet = 'B';
+    // -> method 2 : ASCII Table 안의 정수 값을 사용함.
+    char Alphabet = 98;
+    cout << alphabet << endl;
+    cout << Alphabet << endl;
     return 0;
 }
