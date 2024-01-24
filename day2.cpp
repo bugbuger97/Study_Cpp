c45aa82df4738abf17f79fa9354403016d6f8ceb
bugbuger97 <junopaeng@gmail.com>
2024-01-24 18:47:57 +0900

    implicit type conversion2

--- day2.cpp
+++ day2.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
+#include <typeinfo>
 using namespace std;
 
 int main()
 {
-    // 리터럴 : 값을 나타내지만, 부가 작용(표현식의 결과 값을 변수에 저장함)이 없이 사용할 수 있는 표현식.
-    // <참고> short 리터럴은 없음.
-    // 리터럴(literal) : 변수에 넣는 변하지 않는 데이터를 의미
+    // implicit type conversion
+    bool x = false;
+    char y = 'a';
+    short z = 45;
+    float k = 3.14;
 
-    /*
-    false // bool literal
-    'a' // char literal
-    "Hey" // string literal
-    234 // int literal
-    123=456789L // long literal
-    123.45F // float literal
-    123.45678 // double literal
-    789.10111213L // long double literal
-    */
+    cout << "type of x + 100 : " << typeid(x + 100).name() << endl; // i -> int
+    cout << "value of x + 100 : " << x + 100 << endl;  // 100
+    cout << "type of y + 100 : " <<  typeid(y + 100).name() << endl; // i -> int
+    cout << "value of y + 100 : " << y + 100 << endl; // 197
+    cout << "type of z * 100 : " << typeid(z*100).name() << endl; // i -> int
+    cout << "value of z * 100 : " << z*100 << endl; // 4500
+    cout << "type of k + 100.01 : " << typeid(k + 100.01).name() << endl; // d -> double
+    cout << "value of k + 100.01 : " << k + 100.01 << endl; // 103.15
     return 0;
 }
