2b882e78fb9266ae17c3fa5c24e4a572b45fbda9
bugbuger97 <junopaeng@gmail.com>
2024-01-24 10:55:23 +0900

    overflow_underflow

--- day2.cpp
+++ day2.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
+#include <limits>
 using namespace std;
 
 int main(){
-    // 메모리 변경 == 할당
-    // 부가 작용 == 할당을 해서 메모리에 저장되어 있는 값 변경
-    
-    // explicit type coversion
-    // static_cast<type>(expression)
-    double x = 3.141592;
-    int y = 10;
-    cout << "Not casting : " << x + y << endl; // Not casting : 13.1416
-    cout << "castiong : " << static_cast<int>(x) + y << endl; // castiong : 13
+    // overflow : 기존의 최대값을 넘어섰을 때, 발생.
+    // underflow : 기존의 최솟값보다 더 작아졌을 때, 발생.
+
+    // 부동 소수점의 오버플로우와 언더 플로우
+    double num1 = +numeric_limits<double>::max();
+    double num2 = -numeric_limits<double>::max();
+    cout << "double type max value : " << num1 << endl;
+    cout << "double type min value : " << num2 << endl;
+    num1 *= 1000.00;
+    num2 *= 1000.00;
+    cout << "num1 occurs overflow : " << num1 << endl;
+    cout << "num2 occurs underflow : " << num2 << endl;
     return 0;
 }
