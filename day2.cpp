d51b32c80fe2b65ad34be8ea4eb7c2a795d34bc0
bugbuger97 <junopaeng@gmail.com>
2024-01-24 11:39:11 +0900

    control statement2

--- day2.cpp
+++ day2.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 
 int main(){
-    // declaration
-    int num;
-    // getting input
-    cout << "Enter an integer : ";
-    cin >> num;
-    // control statement
-    if (num < 0){
-        num = -num;
-    }
-    cout << "absolute number : " << num << endl;
+    // 조건문에 괄호를 생략한 경우
+    int x = 0;
+    bool statement1 = true;
+    bool statement2 = false;
+    if (x == 0)
+        cout << statement1 << endl; // statement1 is exexuted if x is 0
+        cout << statement2 << endl; // statement2 is always executed
+    /*
+    if (x == 0) cout << statement1 << endl; 
+    cout << statement2 << endl; // 위의 코드는 이런 식으로 실행됨.
+    */
     return 0;
 }
