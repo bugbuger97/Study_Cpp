2f635c95c7647c83539adaef4825dcdf6894a94b
bugbuger97 <junopaeng@gmail.com>
2024-01-24 09:03:07 +0900

    escape char

--- day1.cpp
+++ day1.cpp
@@ -2,20 +2,13 @@
 using namespace std;
 
 int main(){
-    // 문자 자료형 : 1 byte by ASCII incoding system -> 0 ~ 127 범위의 정수를 사용해서 128개의 글자를 정의하고 있음.
-    char A;
-    char a;
-    A = 65;
-    a = 97;
-    cout << "65 of ASCII : " << A << endl;
-    cout << "97 of ASCII : " << a << endl;
-
-    // 문자 리터널 
-    // -> method 1 : ASCII Table 안의 글자를 작은 따옴표 안에 넣어 사용함.
-    char alphabet = 'B';
-    // -> method 2 : ASCII Table 안의 정수 값을 사용함.
-    char Alphabet = 98;
-    cout << alphabet << endl;
-    cout << Alphabet << endl;
+    // escape char
+    cout << "123\n456" << endl; 
+    cout << "123\t456" << endl; 
+    cout << "123\b456" << endl; // 12456
+    cout << "123\r456" << endl; // 456 -> \r의 커서 앞으로 이동 -> 즉, cout << "456" << endl;로 이동함.
+    cout << "789\f10" << endl; // 789\n 이후 \t10이 실행되는 현상을 느낌.
+    cout << "\'789\'" << endl;
+    cout << "\"789\"" << endl;
     return 0;
 }
