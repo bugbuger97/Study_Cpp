f3def00c2f30a939caacaf15f1b895e383e7e4a4
bugbuger97 <junopaeng@gmail.com>
2024-01-24 13:51:56 +0900

    swich statement

--- day2.cpp
+++ day2.cpp
@@ -2,14 +2,43 @@
 using namespace std;
 
 int main(){
-    // Logic expression
-    int age;
-    bool eligible;
-    cout << "Enter your age : ";
-    cin >> age;
-    eligible = (age >= 25 && age <= 80);
-    if(eligible) cout << "you are eligible to rent to a car";
-    else cout << "you are not eligible to rent to a car";
+    // !(x && y) == (!x || !y)
+    // !(x || y) == (!x && !y)
+
+    // if 블럭과 else 블럭 우치 바꾸기
+    /*
+    // original code
+    if (x){
+    }
+    else {문장};
     
+    // 블럭 위치 바꾸기
+    if (!x){
+        문장;
+    } 
+    else{
+    }
+
+    // 간단하게 만들기
+    if(!x){
+        문장;
+    }
+    */
+
+    // swich statement
+    int day;
+    cout << "Enter a number : ";
+    cin >> day;
+
+    switch(day){
+        case 0: cout << "Sunday" << endl;
+        case 1: cout << "Monday" << endl;
+        case 2: cout << "Tuesday" << endl;
+        case 3: cout << "Wendnesday" << endl;
+        case 4: cout << "Thursday" << endl;
+        case 5: cout << "Friday" << endl;
+        case 6: cout << "Saturday" << endl;
+        default: cout << "vacation" << endl;
+    }
     return 0;
 }
