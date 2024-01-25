20995478fc5a2869acee3559f15e8ba97d10f234
bugbuger97 <junopaeng@gmail.com>
2024-01-25 14:46:33 +0900

    bool manipulator

--- day3.cpp
+++ day3.cpp
@@ -1,31 +1,19 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
-// int main(){
-//     const double PI = 3.141592653589793238462;
-//     double degree = PI / 6;
-//     cout << "sin(45) = " << sin(degree) << endl;
-//     cout << "cos(45) = " << cos(degree) << endl;
-//     cout << "tan(45) = " << tan(degree) << endl;
-//     return 0;
-// }
+// endl 조정자 : \n 조정자를 추가하여 줄바꿈을 하게 설계됨.
+// bool(noboolalpha, boolalpha)
+// noalpha 조정자 : bool 값을 0 또는 1로 출력함. 
+// boolalpha 조정자 : bool 값을 리터럴(false 또는 True)로 출력함.
 
-int main() {
-    const double PI = 3.141592653589793238462;
-    int num;
-    double side, peri, area;
-    do{
-        cout << "Input number of sides(more than four) : ";
-        cin >> num;
-    } while(num < 4);
-    do{
-        cout << "Input length of sides : ";
-        cin >> side;
-    }while(side<=0.0);
-    peri = num * side;
-    area = (num * pow(side,2)) / (num * tan(PI / num));
-    cout << "peri : " << peri << endl;
-    cout << "area : " << area << endl;
+int main(){
+    bool a = true;
+    bool b = false;
+    // 조정자 없이 출력
+    cout << "Print of original a : " << a << endl;
+    cout << "Print of original b : " << b << endl;
+    // 조정자를 사용하여 출력
+    cout << "Print a using manipulator : " << boolalpha << a << endl; // bool 조정자 한번만 사용하면 계속 적용됨.
+    cout << "Print b using manipulator : " << b << endl;
     return 0;
 }
