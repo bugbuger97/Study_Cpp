6635c445017dd5d02cc0911522a2611a494d9208
bugbuger97 <junopaeng@gmail.com>
2024-01-25 14:07:59 +0900

    cmath

--- day3.cpp
+++ day3.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int func(int x){ // 매개변수
-    return x;
-}
+// int main(){
+//     const double PI = 3.141592653589793238462;
+//     double degree = PI / 6;
+//     cout << "sin(45) = " << sin(degree) << endl;
+//     cout << "cos(45) = " << cos(degree) << endl;
+//     cout << "tan(45) = " << tan(degree) << endl;
+//     return 0;
+// }
 
-int main(){
-    // parameter : 변수를 할당할 때, 왼쪽에 위치하는 변수
-    // argument : 오른쪽에 위치하는 값
-    int a = 5;
-    cout << func(a); // 인수
+int main() {
+    const double PI = 3.141592653589793238462;
+    int num;
+    double side, peri, area;
+    do{
+        cout << "Input number of sides(more than four) : ";
+        cin >> num;
+    } while(num < 4);
+    do{
+        cout << "Input length of sides : ";
+        cin >> side;
+    }while(side<=0.0);
+    peri = num * side;
+    area = (num * pow(side,2)) / (num * tan(PI / num));
+    cout << "peri : " << peri << endl;
+    cout << "area : " << area << endl;
     return 0;
 }
