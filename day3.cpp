7a035f78e758001bd5fe3f0579680ff0ca52eba1
bugbuger97 <junopaeng@gmail.com>
2024-01-25 13:13:11 +0900

    parameter_argument

--- day3.cpp
+++ day3.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 using namespace std;
 
-// 함수 : 모듈화를 시킬 수 있는 기본적인 단위
-// 함수의 장점 : 분할 처리, 디버그, 재사용, 함수 라이브러리
-// 함수의 이름이 중복이고, 매개변수도 똑같이 되면 안된다. 
-int add(int a, int b) // 헤더
-{ // 2개의 매개변수를 설정
-    return a+b; // 매개변수
+int func(int x){ // 매개변수
+    return x;
 }
 
 int main(){
-    int x = 5;
-    int y = 25;
-    cout << add(x,y); // x, y라는 인수를 add()함수로 전달.
+    // parameter : 변수를 할당할 때, 왼쪽에 위치하는 변수
+    // argument : 오른쪽에 위치하는 값
+    int a = 5;
+    cout << func(a); // 인수
     return 0;
 }
