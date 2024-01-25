20cde55f7dddace44c9c746b9b27729ab787183c
bugbuger97 <junopaeng@gmail.com>
2024-01-25 15:08:02 +0900

    showbase

--- day3.cpp
+++ day3.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 using namespace std;
 
-// endl 조정자 : \n 조정자를 추가하여 줄바꿈을 하게 설계됨.
-// bool(noboolalpha, boolalpha)
-// noalpha 조정자 : bool 값을 0 또는 1로 출력함. 
-// boolalpha 조정자 : bool 값을 리터럴(false 또는 True)로 출력함.
+// 숫자의 진법을 변경하는 조정자(dec, oct, hex)
+// 진법의 접두사를 붙이는 조정자(nonshowbase, showbase)
+// 10진수는 아무 것도 붙지 않고, 8진수는 0, 16진수는 0x가 접두사로 붙어서 각각의 진법을 나타냄.
 
 int main(){
-    bool a = true;
-    bool b = false;
-    // 조정자 없이 출력
-    cout << "Print of original a : " << a << endl;
-    cout << "Print of original b : " << b << endl;
-    // 조정자를 사용하여 출력
-    cout << "Print a using manipulator : " << boolalpha << a << endl; // bool 조정자 한번만 사용하면 계속 적용됨.
-    cout << "Print b using manipulator : " << b << endl;
+    int a = 16;
+    // showbase 없이 3개의 숫자를 진법별로 입력하기.
+    cout << "dec of a : " << a << endl;
+    cout << "oct of a : " << oct << a << endl;
+    cout << "hax of a : " << hex << a << endl;
+    // showbase를 사용한 상태로 x를 각각의 진법으로 출력
+    cout << "dec of a : " << showbase << dec << a << endl;
+    cout << "oct of a : " << showbase << oct << a << endl;
+    cout << "hax of a : " << showbase << hex << a << endl;  
     return 0;
 }
