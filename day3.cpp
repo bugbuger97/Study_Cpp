d04a63f562d68b499bf4522c53f600b8caa19912
bugbuger97 <junopaeng@gmail.com>
2024-01-25 18:26:31 +0900

    current_time

--- day3.cpp
+++ day3.cpp
@@ -1,25 +1,19 @@
 #include <iostream>
-#include <typeinfo>
+#include <ctime>
 using namespace std;
 
-// 팩토리얼
-int factorial(int num){
-    if (num == 1 || num == 0){
-        return 1;
-    }
-    else{
-        return num * factorial(num-1);
-    } 
-}
-    
-
-int main()
-{
-    int num;
-    do{
-        cout << "Input number : ";
-        cin >> num;
-    }while(num < 0);
-    cout << num << "! = " << factorial(num) << endl;
+// 시간 함수
+int main(){
+    long elapsed_second = time(0);
+    int current_second = elapsed_second % 60;
+    // 경과한 분 단위 시간과 현재 분 찾기
+    long elapsed_minute = elapsed_second / 60;
+    int current_minute = elapsed_minute % 60;
+    // 경과한 시간과 시 단위 시간 찾기
+    long elapsed_hour = elapsed_minute / 60;
+    int current_hour = elapsed_hour % 24;
+    // 현재 시간을 출력
+    cout << "current time = ";
+    cout << current_hour << " : " << current_minute << " : " << current_second << endl;
     return 0;
 }
