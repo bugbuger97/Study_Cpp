d2537fce87233b84464ad5e247fbd0f3ea607198
bugbuger97 <junopaeng@gmail.com>
2024-01-25 18:05:27 +0900

    factorial

--- day3.cpp
+++ day3.cpp
@@ -2,23 +2,16 @@
 #include <typeinfo>
 using namespace std;
 
-// return 구문은 현재 함수를 종료하고 함수를 호출한 쪽으로 제어를 리턴함.
-// return 구문을 사용하면, 반복문이 종료되며, 동시에 반복문이 소속된 함수(main 함수 등)도 종료됨.
-
-// 소수인지 확인하는 프로그램
-bool isPrime(int num){
-    if (num == 1){
-        return false;
+// 팩토리얼
+int factorial(int num){
+    if (num == 1 || num == 0){
+        return 1;
     }
     else{
-        for (int i=2; i<num; i++){
-            if (num % i == 0){
-                return false;
-            }
-        }
-    }
-    return true;
+        return num * factorial(num-1);
+    } 
 }
+    
 
 int main()
 {
@@ -26,13 +19,7 @@ int main()
     do{
         cout << "Input number : ";
         cin >> num;
-    }while(num <= 0);
-
-    if (isPrime(num)){
-        cout << num << " is Prime number!" << endl;
-    }
-    else{
-        cout << num << " is not Prime number!" << endl;
-    }
+    }while(num < 0);
+    cout << num << "! = " << factorial(num) << endl;
     return 0;
 }
