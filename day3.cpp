d71dbfdda5b6c8d5a1fdfc4462be2ff9eb3f41f5
bugbuger97 <junopaeng@gmail.com>
2024-01-25 10:40:54 +0900

    Create a calendar

--- day3.cpp
+++ day3.cpp
@@ -1,19 +1,39 @@
 #include <iostream>
-#include <fstream>
+#include <iomanip>
 using namespace std;
 
 int main(){
-    // Using EOF in a file
-    int sum = 0;
-    int num;
-    ifstream infile;
-    // Open a file
-    infile.open("numbers.txt");
-    // loop
-    while(infile >> num){
-        sum += num;
+    // declaration
+    int start_day;
+    int days_in_month;
+    int col = 1;
+    // validation of days in a month
+    do{
+        cout << "Enter the number of day (28,29,30,31) : ";
+        cin >> days_in_month;
+    }while(days_in_month < 28 || days_in_month > 31);
+    // 요일 유효성 검사
+    do{
+        cout << "Input first day of week(0 ~ 6) : ";
+        cin >> start_day;
+    }while(start_day < 0 || start_day > 6);
+    // 제목 출력
+    cout << endl;
+    cout << "Son Mon Tue Wed Thr Fri Sat" << endl;
+    cout << "--- --- --- --- --- --- ---" << endl;
+    // 달력의 앞쪽 여백 출력
+    for (int space = 0; space < start_day; space++){
+        cout << "    ";
+        col++;
     }
-    cout << "sum = " << sum;
-    infile.close();
+    // 달력 출력
+    for (int day = 1; day <= days_in_month; day++){
+        cout << setw(3) << day << " ";
+        col++;
+        if (col > 7){
+            cout << endl;
+            col = 1;
+        }
+    }
     return 0;
 }
