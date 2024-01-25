4887cfca798b1182f190a183cc063c04e9829253
bugbuger97 <junopaeng@gmail.com>
2024-01-25 09:54:30 +0900

    Using EOF in a file

--- day3.cpp
+++ day3.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
-#include <typeinfo>
+#include <fstream>
 using namespace std;
 
 int main(){
-    // explicit type conversion(casting)
-
-    // declaration
-    float pi = 3.14;
-    int y = 3;
-    // implicit type casting
-    cout << "implicit type casting : " << pi + y << endl;
-    cout << "a type of pi + y : " << typeid(pi + y).name() << endl; // float
-    cout << "explicit type casting : " << static_cast<int>(pi) + y << endl;
-    cout << "a type of pi + y : " << typeid(static_cast<int>(pi) + y).name() << endl; // int
+    // Using EOF in a file
+    int sum = 0;
+    int num;
+    ifstream infile;
+    // Open a file
+    infile.open("numbers.txt");
+    // loop
+    while(infile >> num){
+        sum += num;
+    }
+    cout << "sum = " << sum;
+    infile.close();
     return 0;
 }
