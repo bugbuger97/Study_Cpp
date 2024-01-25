e3d09ba0ef83bc3717c23c155f1fd1569693e903
bugbuger97 <junopaeng@gmail.com>
2024-01-25 16:15:37 +0900

    fixed_format and scientific_format

--- day3.cpp
+++ day3.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 using namespace std;
 
-// 숫자의 진법을 변경하는 조정자(dec, oct, hex)
-// 진법의 접두사를 붙이는 조정자(nonshowbase, showbase)
-// 10진수는 아무 것도 붙지 않고, 8진수는 0, 16진수는 0x가 접두사로 붙어서 각각의 진법을 나타냄.
+// 고정 소수점 출력 조정자와 과학 표기법 출력 조정자(fixed, scientific)
 
+// 1. 부동 소수점 자료형을 출력할 때, 고정 소수점(fixed format)을 사용하는 방법
+// 고정 소수점 형식
+// 부호 / 정수 부분 / . / 소수점 아래 부분 
+
+// 2. 부동 소수점 자료형을 출력할 때, 과학 표기법(scientific) 표기법을 사용하는 방법
+// 과학 표기법 형식
+// 부호 / 정수 부분이 한자리로 고정 / e 또는 E / 지수 부호 / 지수
+
+// 소수점 아래 부분을 출력하는 조정자(showpoint)
 int main(){
-    int a = 16;
-    // showbase 없이 3개의 숫자를 진법별로 입력하기.
-    cout << "dec of a : " << a << endl;
-    cout << "oct of a : " << oct << a << endl;
-    cout << "hax of a : " << hex << a << endl;
-    // showbase를 사용한 상태로 x를 각각의 진법으로 출력
-    cout << "dec of a : " << showbase << dec << a << endl;
-    cout << "oct of a : " << showbase << oct << a << endl;
-    cout << "hax of a : " << showbase << hex << a << endl;  
+    // declaration
+    double a = 144;
+    double b = 314.1592;
+    // fixed와 showpoint 조정자 사용
+    cout << "Fixed format of a : " << a << endl;
+    cout << "Fixed format of a : " << showpoint << a << endl;
+    // scientific 조정자 사용
+    cout << "Scientific format of b : " << scientific << b << endl;
     return 0;
 }
