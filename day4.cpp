8649f6069ef6c2713a5677936e4022ac5ea84633
bugbuger97 <junopaeng@gmail.com>
2024-01-26 15:50:48 +0900

    Constructor with 2 parameter

--- day4.cpp
+++ day4.cpp
@@ -1,55 +1,41 @@
+// 정적 데이터 멤버 선언
+// 전역 변수 같은 것인가?
+/*
+정적 데이터 멤버(static data member)는 클래스의 인스턴스가 아닌 클래스 자체에 속하는 멤버 변수임. 
+따라서 이러한 변수는 클래스의 모든 인스턴스에서 동일한 값을 공유하게 됨.
+정적 데이터 멤버는 전역 변수처럼 사용될 수 있음. 
+하나의 클래스에서 생성된 모든 객체가 해당 정적 데이터 멤버를 공유하므로 클래스 간에 데이터를 공유하는 용도로 사용할 수 있음. 
+클래스의 인스턴스 없이도 접근이 가능하며, 클래스 이름을 통해 접근할 수 있음.
+*/
+
+// 매개 변수가 있는 생성자
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include <cassert>
 using namespace std;
 
-// 클래스 정의
-class RandomInteger{
+class Fraction{
     private:
-        // 데이터 멤버
-        int low; 
-        int high;
-        int value;
+        int num;
+        int den;
     public:
-        // 생성자
-        RandomInteger(int low, int high);
-        // 소멸자
-        ~RandomInteger();
-        // 합성 복사 생성자의 생성 막기
-        RandomInteger(const RandomInteger& random) = delete;
-        void print() const; // 접근자 멤버 함수
+        // default constructor
+        Fraction(){
+            num = 0;
+            den = 1;
+        }
+        // Constructor with 2 parameter
+        Fraction(int n, int d = 1){
+            assert(d!=0);
+            num = n;
+            den = d;
+        }
+        int get_num() {return num;}
+        int get_den() {return den;}
+        double get_val() {return static_cast<double>(num) / den;}
 };
-// 클래스 안의 생성자, 소멸자, 접근자 멤버 함수 정의
 
-// 생성자
-RandomInteger::RandomInteger(int low, int high)
-:low(low), high(high)
-{
-    srand(time(0));
-    int temp = rand();
-    value = temp % (high - low + 1) + low;
-}
-
-// 소멸자
-RandomInteger::~RandomInteger()
-{
-}
-
-// 접근자 멤버 함수
-void RandomInteger::print() const{
-    cout << value << endl;
-}
-
-// 애플리케이션
 int main(){
-    RandomInteger r1(100,200);
-    cout << "random number of 100 ~ 200 : ";
-    r1.print();
-    RandomInteger r2(500,1000);
-    cout << "random number of 500 ~ 1000 : ";
-    r2.print();
-    RandomInteger r3(50,100);
-    cout << "random number of 50 ~ 100 : ";
-    r3.print();
+    int x(5);
+    Fraction twotwo(2,2);
     return 0;
 }
