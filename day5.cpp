ef22569f695aff6c55dc054e8ffe1b14598aff38
bugbuger97 <junopaeng@gmail.com>
2024-01-29 10:40:53 +0900

    array

--- day5.cpp
+++ day5.cpp
@@ -1,22 +1,18 @@
 #include <iostream>
 using namespace std;
 
-// pass-by-pointer
-// 포인터로 전달 메커니즘은 인수로 메모리 주소를 매개변수에 전달하는 것임.
-// 주소를 전달하므로 매개변수를 사용해서 인수의 메모리 위치에 접근할 수 있음.
-// c++에서는 참조로 전달 메커니즘이 있어서, 포인터로 전달 메커니즘은 거의 사용되지 않음.
-// 포인터로 전달 메커니즘은 참조로 전달 메커니즘과 같은 특징을 가짐.
+// return-by-reference
+// 복사로 인해서 발생하는 비용을 줄이기 위해 참조로 리턴을 하는 것이 좋음.
+// 그러나, 호출되는 함수에서 객체를 생성하면, 함수가 종료된 이후에는 객체가 사라짐.
+// 그렇기 때문에, 참조로 리턴을 할 때는 조심해야함.
 
-// return-by-value
-// 호출되는 함수쪽에서 어떤 표현식을 생성하고, 이를 리턴함.
-// 함수를 호출하면 값이 리턴되기에, 값이 필요한 위치에 함수를 활용함.
-bool isOdd(int num){
-    return (num % 2 == 1); // 리터럴 값 리턴
-    // 리터럴 :  코드의 고정된 값을 나타내는 표기법
-}
+// return-by-pointer : 사용하지 않음.
 
+// array
 int main(){
-    cout << boolalpha << isOdd(2) << endl;
-    cout << boolalpha << isOdd(9) << endl;
+    int i = 1; 
+    int scores[i]={1}; // 여기서 i는 const로 지정해서 들어간다. 
+    //i가 run time 중에 변화한 시도가 있으면, const로 취급하지 못해서 error가 발생함.
+    cout << scores[0] << endl;
     return 0;
 }
