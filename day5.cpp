2580fdf1cb3690d388401f4b01e0b9f28b204839
bugbuger97 <junopaeng@gmail.com>
2024-01-29 16:18:40 +0900

    function overloading

--- day5.cpp
+++ day5.cpp
@@ -1,18 +1,41 @@
 #include <iostream>
 using namespace std;
 
-// return-by-reference
-// 복사로 인해서 발생하는 비용을 줄이기 위해 참조로 리턴을 하는 것이 좋음.
-// 그러나, 호출되는 함수에서 객체를 생성하면, 함수가 종료된 이후에는 객체가 사라짐.
-// 그렇기 때문에, 참조로 리턴을 할 때는 조심해야함.
+// function overloading
+// 이름이 같은 함수가 2개 이상일 때, 매개변수의 자료형, 개수, 순서가 다르다면 사용 가능함. -> 함수 오버로딩
+int min(int a, int b){
+    if (a > b){
+        return b;
+    }
+    else{
+        return a;
+    }
+}
+int min(int a, int b, int c){
+    int arr[3] = {a,b,c};
+    int temp = arr[0];
+    for (int i=0; i < 3; i++){
+        if (temp > arr[i]){
+            temp = arr[i];
+        }
+    }
+    return temp;
+}
 
-// return-by-pointer : 사용하지 않음.
+int min(int a, int b, int c, int d){
+    int arr[4] = {a,b,c,d};
+    int temp = arr[0];
+    for (int i=0; i < 3; i++){
+        if (temp > arr[i]){
+            temp = arr[i];
+        }
+    }
+    return temp;
+}
 
-// array
 int main(){
-    int i = 1; 
-    int scores[i]={1}; // 여기서 i는 const로 지정해서 들어간다. 
-    //i가 run time 중에 변화한 시도가 있으면, const로 취급하지 못해서 error가 발생함.
-    cout << scores[0] << endl;
+    cout << "min(5,7) = " << min(5,7) << endl;
+    cout << "min(10,1,9) = " << min(10,1,9) << endl;
+    cout << "min(100,99,67,87) = " << min(100,99,67,87) << endl;
     return 0;
 }
