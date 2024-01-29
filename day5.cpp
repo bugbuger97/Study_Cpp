c1afd4602faf325f8f4640853802472b0d8f9bf4
bugbuger97 <junopaeng@gmail.com>
2024-01-29 16:35:56 +0900

    scope

--- day5.cpp
+++ day5.cpp
@@ -1,41 +1,14 @@
 #include <iostream>
 using namespace std;
 
-// function overloading
-// 이름이 같은 함수가 2개 이상일 때, 매개변수의 자료형, 개수, 순서가 다르다면 사용 가능함. -> 함수 오버로딩
-int min(int a, int b){
-    if (a > b){
-        return b;
-    }
-    else{
-        return a;
-    }
-}
-int min(int a, int b, int c){
-    int arr[3] = {a,b,c};
-    int temp = arr[0];
-    for (int i=0; i < 3; i++){
-        if (temp > arr[i]){
-            temp = arr[i];
-        }
-    }
-    return temp;
-}
+// scope : 소스 코드 내부에서 어떤 entity(상수, 변수, 객체, 함수) 사용할 수 있는 범위를 나타냄.
+// local scope : entity of local scope는 선언된 위치 ~ 블록이 끝나는 부분(줄괄호 끝) 내부에서 사용할 수 있음.
 
-int min(int a, int b, int c, int d){
-    int arr[4] = {a,b,c,d};
-    int temp = arr[0];
-    for (int i=0; i < 3; i++){
-        if (temp > arr[i]){
-            temp = arr[i];
-        }
-    }
-    return temp;
-}
+// scope resolution operator
 
-int main(){
-    cout << "min(5,7) = " << min(5,7) << endl;
-    cout << "min(10,1,9) = " << min(10,1,9) << endl;
-    cout << "min(100,99,67,87) = " << min(100,99,67,87) << endl;
-    return 0;
+int num = 10; // 전역 변수
+int main(){ 
+    int num = 500; // 지역 변수
+    cout << "global var = " << ::num << endl; // 10
+    cout << "logical var = " << num << endl; // 500
 }
